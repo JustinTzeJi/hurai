fba7de777c22313442b6439aa2ddbed808978bb1
justintzeji <justintzeji@gmail.com>
2025-03-10 10:05:07 +0800

    update: improve dataclasses and implement api keys

--- api/index.py
+++ api/index.py
@@ -2,33 +2,56 @@ import os
 import requests
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Security, status
+from fastapi.security import APIKeyHeader
+
 load_dotenv()
 
+api_key_header = APIKeyHeader(name="X-API-Key")
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
 
 class alt_text(BaseModel):
     alt_text: str
 
     model_config = {
-        "json_schema_extra": {
-            "examples": [{
-        "alt_text": "A rabbit in a grass field"
-    }]}}
+        "json_schema_extra": {"examples": [{"alt_text": "A rabbit in a grass field"}]}
+    }
+
+class dual_lang(BaseModel):
+    ms: str
+    en: str
 
 class alt_text_translated(BaseModel):
-    alt_text: str
+    alt_text: dual_lang
 
     model_config = {
         "json_schema_extra": {
-            "examples": [{
-        "alt_text": """Seekor arnab di padang rumput
-A rabbit in a grass field"""
-    }]}}
+            "examples": [
+                {
+                    "alt_text": {"ms":"Seekor arnab di padang rumput",
+                                 "en":"A rabbit in a grass field"}
+                }
+            ]
+        }
+    }
+
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    api_keys = os.getenv("API_KEYS").split(",")
+    if api_key_header in api_keys:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+
 
 @app.post("/api/py/generate-caption-plain")
-def gen_caption_plain(file: UploadFile) -> alt_text:
+def gen_caption_plain(
+    file: UploadFile, api_key: str = Security(get_api_key)
+) -> alt_text:
     API_URL = "https://router.huggingface.co/hf-inference/models/Salesforce/blip-image-captioning-base"
     headers = {"Authorization": f"Bearer {os.getenv('HUGGINGFACE_INF_KEY')}"}
     try:
@@ -43,8 +66,9 @@ def gen_caption_plain(file: UploadFile) -> alt_text:
     response = requests.post(API_URL, headers=headers, data=contents)
     return alt_text(alt_text=response.json()[0]["generated_text"])
 
+
 @app.post("/api/py/translate-my")
-def translate_my(alt_text) -> alt_text_translated:
+def translate_my(alt_text, api_key: str = Security(get_api_key)) -> alt_text_translated:
     API_URL = "https://api.mesolitica.com/translation/public"
     headers = {"Content-Type": "application/json"}
 
@@ -55,14 +79,17 @@ def translate_my(alt_text) -> alt_text_translated:
         "top_k": 1,
         "top_p": 1,
         "repetition_penalty": 1.1,
-        "temperature": 0
+        "temperature": 0,
     }
 
     response = requests.post(API_URL, headers=headers, json=payload)
-    return alt_text_translated(alt_text=response.json()["result"]+"\n"+alt_text)
+    return alt_text_translated(alt_text={"ms":response.json()["result"], "en": alt_text})
+
 
 @app.post("/api/py/generate-caption")
-def gen_caption(file: UploadFile) -> alt_text_translated:
+def gen_caption(
+    file: UploadFile, api_key: str = Security(get_api_key)
+) -> alt_text_translated:
     alt_gen = gen_caption_plain(file)
     alt_gen_final = translate_my(alt_gen.alt_text)
     return alt_gen_final
